010395a88a68b6613e914c730a68be48780f670c
Carlos <carlos@CarlosdeMacBook-Pro.local>
2023-01-21 14:57:41 +0800

    发布

--- src/services/service.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const serve = axios.create({
-  baseURL: "https://",
-  timeout: 10000,
-});
--- src/services/tool.js
+++
@@ -1,9 +0,0 @@
-import { serve } from "./service";
-
-export async function getExpressDetail(params){
-    return serve.get(`api.vvhan.com/api/kuaidi`,{
-        params: {
-            ...params
-        }
-    })
-}
